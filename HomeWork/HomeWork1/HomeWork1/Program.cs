7bcdfc950d0070403cd3861a005a1598d5924b50
BajcoXD <viktor.gaming2018@gmail.com>
2025-04-30 00:55:39 +0200

    Task 2

--- HomeWork/HomeWork1/HomeWork1/Program.cs
+++ HomeWork/HomeWork1/HomeWork1/Program.cs
@@ -1,9 +1,9 @@
 ﻿using HomeWork1.Entities.Models;
 
 Searchable doc = new Document("This is a sample document containing various words.");
-Searchable page = new WebPage("<html><body>Welcome to our webpage about technology and science.</body></html>");
+Searchable page = new WebPage("I know who Viktor is!");
 
-string keyword = "technology";
+string keyword = "Viktor";
 
 Console.WriteLine($"Keyword '{keyword}' found in document? {doc.Search(keyword)}");
 Console.WriteLine($"Keyword '{keyword}' found in webpage? {page.Search(keyword)}");
---
+++ HomeWork/HomeWork1/HomeWork1/Task2/Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿using Task2.Entities.Models;
+
+Shape rectangle = new Rectangle(5, 4);
+Shape circle = new Circle(3);
+
+
+Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
+Console.WriteLine($"Circle Area: {circle.GetArea():F2}");
