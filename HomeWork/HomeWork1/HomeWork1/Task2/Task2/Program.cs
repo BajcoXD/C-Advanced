f9ec09d4224fe1e5ceafa783d442cfe640f09072
Viktor Stojanovski <viktor.gaming2018@gmail.com>
2025-04-30 01:11:31 +0200

    Delete HomeWork/HomeWork1 directory

--- HomeWork/HomeWork1/HomeWork1/Task2/Task2/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Task2.Entities.Models;
-
-Shape rectangle = new Rectangle(5, 4);
-Shape circle = new Circle(3);
-
-
-Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
-Console.WriteLine($"Circle Area: {circle.GetArea():F2}");
