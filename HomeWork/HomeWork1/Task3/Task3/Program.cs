069e309dc81ed5e390ee1c7a38888721a24f5907
BajcoXD <viktor.gaming2018@gmail.com>
2025-04-30 01:03:23 +0200

    Task 3

---
+++ HomeWork/HomeWork1/Task3/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿using Task3.Entities.Models;
+
+Shape circle = new Circle(5);
+Shape triangle = new Triangle(3, 4, 5);
+
+Console.WriteLine("Circle:");
+Console.WriteLine($"Area: {circle.CalculateArea():F2}");
+Console.WriteLine($"Perimeter: {circle.CalculatePerimeter():F2}");
+
+Console.WriteLine("\nTriangle:");
+Console.WriteLine($"Area: {triangle.CalculateArea():F2}");
+Console.WriteLine($"Perimeter: {triangle.CalculatePerimeter():F2}");
