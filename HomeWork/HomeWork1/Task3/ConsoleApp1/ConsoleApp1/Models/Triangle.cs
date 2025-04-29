329b9a381721a7f41e0caf52846ab503f0c58b42
BajcoXD <viktor.gaming2018@gmail.com>
2025-04-30 01:19:52 +0200

    Task

---
+++ HomeWork/HomeWork1/Task3/ConsoleApp1/ConsoleApp1/Models/Triangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    public class Triangle : Shape
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public Triangle(double a, double b, double c)
+        {
+            SideA = a;
+            SideB = b;
+            SideC = c;
+        }
+
+        public override double CalculateArea()
+        {
+            // Heron's formula
+            double s = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return SideA + SideB + SideC;
+        }
+    }
+}
