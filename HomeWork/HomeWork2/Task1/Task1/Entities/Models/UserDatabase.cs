4060423da17ef414d09585ba586bc67939156aa3
BajcoXD <viktor.gaming2018@gmail.com>
2025-05-02 18:19:58 +0200

    Task 1

---
+++ HomeWork/HomeWork2/Task1/Task1/Entities/Models/UserDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Entities.Models
+{
+    public static class UserDatabase
+    {
+        public static List<User> users = new List<User>
+        {
+             new User(1, "Alice", 25),
+             new User(2, "Bob", 30),
+             new User(3, "Charlie", 25)
+        };
+        public static User SearchById(int id)
+        {
+            return users.FirstOrDefault(u => u.Id == id);
+        }
+        public static List<User> SearchByName(string name)
+        {
+            return users.Where(u => u.Name.Equals(name, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        public static List<User> SearchByAge(int age)
+        {
+            return users.Where(u => u.Age == age).ToList();
+        }
+
+        public static void PrintUsers(List<User> users)
+        {
+            if (users.Count == 0)
+            {
+                Console.WriteLine("No users found.");
+            }
+            else
+            {
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user);
+                }
+            }
+        }
+
+    }
+}
---
+++ HomeWork/HomeWork2/Task1/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿using Task1.Entities.Models;
+
+Console.WriteLine("Search by Id:");
+Console.WriteLine(UserDatabase.SearchById(2));
+
+Console.WriteLine("\nSearch by Name:");
+UserDatabase.PrintUsers(UserDatabase.SearchByName("alice"));
+
+Console.WriteLine("\nSearch by Age:");
+UserDatabase.PrintUsers(UserDatabase.SearchByAge(25));
