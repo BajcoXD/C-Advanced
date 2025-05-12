7891355801b6ca9b98c7f9536f83b81fc0d687ef
BajcoXD <viktor.gaming2018@gmail.com>
2025-05-12 14:37:11 +0200

    Task 1

---
+++ HomeWork/HomeWork3/Task1/app/Models/PrintInConsole.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.Models
+{
+    public class PrintInConsole
+    {
+        
+        public void Print<T>(T item)
+        {
+            Console.WriteLine(item);
+        }
+
+        
+        public void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ HomeWork/HomeWork3/Task1/app/Program.cs
@@ -0,0 +1,13 @@
+﻿using app.Models;
+
+var printer = new PrintInConsole();
+
+printer.Print("Hello World!");
+printer.Print(123);
+printer.Print(45.67);
+
+List<string> names = new List<string> { "Viktor", "Bob", "Bobski" };
+printer.PrintCollection(names);
+
+int[] numbers = { 1, 2, 3, 4, 5 };
+printer.PrintCollection(numbers);
